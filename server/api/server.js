1b32b009f177c6400418866c51ee23088081ae38
Daniel Petrie <danielpetrie07@gmail.com>
2024-01-12 22:23:24 +1300

    updated database

--- server/api/server.js
+++ server/api/server.js
@@ -1,7 +1,7 @@
   const express = require('express')
   const cors = require('cors')
   const helmet = require('helmet')
-  const knex = require('knex')
+  const db = require('./dbConfig')
 
   const server = express()
 
@@ -11,11 +11,12 @@
 
   const userExists = async (auth0Id) => {
     try {
-      console.log('checking user existence')
-      const user = await knex('users').where({ auth0Id }).first();
-      return !!user;
+     
+      const user = await db('users').where( 'auth0Id', auth0Id ).first();
+     
+      return !user;
     } catch (error) {
-      console.error(error);
+   
       throw error;
     }
   };
@@ -23,33 +24,34 @@
 
   server.get('/', (req, res) => {
     res.send('Welcome to greed!!');
-    console.log('POST /', req);
+    
   });
 
   server.get('/api/hello', (req, res) => {
-    console.log('POST /api/hello', req);
+    
     res.send('Hello from the server!' );
   });
 
   server.post('/api/register', async (req, res) => {
-    console.log('POST /api/register', req);
+    
     const { sub, email, nickname } = req.body; // Removed the extra parenthesis
 
     const isUserRegistered = await userExists(sub);
 
     if (isUserRegistered) {
-      console.log('User is already registered');
-
-      knex('users').insert({ auth0Id: sub, email, username: nickname })
+      db('users').insert({ auth0Id: sub, email, displayName: nickname })
         .then(() => {
           res.status(201).json({ success: true, message: 'User registered successfully' });
         })
         .catch((error) => {
           console.error(error);
           res.status(500).json({ success: false, message: 'Internal Server Error' });
-        });
-    }
-  });
+        })
+    }else {
+     
+      res.status(200).json({ success: false, message: 'User already registered' });
+  
+  }});
 
   server.get('/api/user/:userId', (req, res) => {
     // Handle getting a specific user
